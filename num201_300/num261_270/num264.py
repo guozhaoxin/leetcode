24b7cd53e6b4859813532fcaf22b6bfb0763b1ec
guozhaoxin <1345616978@qq.com>
2018-10-17 13:40:57 +0800

    gold,20181017 add 264 but not solved.

--- num201_300/num261_270/num264.py
+++ num201_300/num261_270/num264.py
@@ -2,15 +2,62 @@
 __author__ = 'gold'
 
 '''
+264.
+Ugly Number II
 
+Write a program to find the n-th ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. 
+
+Example:
+
+Input: n = 10
+Output: 12
+Explanation: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
+1 is typically treated as an ugly number.
+n does not exceed 1690.
 '''
 
-def haha(n):
-    result = [1]
-    if n == 1:
-        return result
+class Solution:
+    def nthUglyNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        results = [1,2,3]
+        if n <= 3:
+            return results[:n][-1]
+        l1 = [0,[5]]
+        l2 = [1,[2,3,5]]
+        l3 = [2,[2,3,5]]
 
-    left = 0
-    while len(result) < n:
-        temp = result[-1]
-        temp *= 2
+        while len(results) < n:
+            first = results[l1[0]] * l1[1][0]
+            second = results[l2[0]] * l2[1][0]
+            third = results[l3[0]] * l3[1][0]
+            haha = min(first,second,third)
+            results.append(haha)
+            if first == haha:
+                del l1[1][0]
+                if len(l1[1]) == 0:
+                    l1 = [max(l1[0],l2[0],l3[0]) + 1,[2,3,5]]
+                    while results[l1[0]] * l1[1][0] <= haha:
+                        del l1[1][0]
+            if second == haha:
+                del l2[1][0]
+                if len(l2[1]) == 0:
+                    l2 = [max(l1[0],l2[0],l3[0]) + 1,[2,3,5]]
+                    while results[l2[0]] * l2[1][0] <= haha:
+                        del l2[1][0]
+
+            if third == haha:
+                del l3[1][0]
+                if len(l3[1]) == 0:
+                    l3 = [max(l1[0],l2[0],l3[0]) + 1,[2,3,5]]
+                    while results[l3[0]] * l3[1][0] <= haha:
+                        del l3[1][0]
+
+        return results[-1]
+
+if __name__ == '__main__':
+    print(Solution().nthUglyNumber(12))
